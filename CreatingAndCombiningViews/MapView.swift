f91d8d4492e54faa258e21f7503415876a16c6e8
akshay1992kalbhor <akshay1992kalbhor@gmail.com>
2020-01-26 17:11:21 +0530

    Use UIKit and SwiftUI Views Together

---
+++ CreatingAndCombiningViews/MapView.swift
@@ -0,0 +1,29 @@
+//
+//  MapView.swift
+//  CreatingAndCombiningViews
+//
+//  Created by Akshay Kalbhor on 1/26/20.
+//  Copyright © 2020 Akshay Kalbhor. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
