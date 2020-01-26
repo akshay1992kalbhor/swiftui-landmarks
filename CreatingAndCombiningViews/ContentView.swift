86bf47ba4a707e8d47f36a24af69f26f0029bd71
akshay1992kalbhor <akshay1992kalbhor@gmail.com>
2020-01-26 16:21:41 +0530

    Initial Commit

---
+++ CreatingAndCombiningViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CreatingAndCombiningViews
+//
+//  Created by Akshay Kalbhor on 1/26/20.
+//  Copyright © 2020 Akshay Kalbhor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
